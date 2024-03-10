00c2ee3ac1c4b092f3be7ffe8690af098830af45
Joshua Lafleur <77811114+JoshLafleur@users.noreply.github.com>
2024-03-10 18:07:41 -0400

    Revert "..."
    This reverts commit 8958d72be30b86440668d78f4d1cb73f06e3ac13.

--- app/src/main/java/com/example/candroid_diagnostics/Globals.java
+++ app/src/main/java/com/example/candroid_diagnostics/Globals.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 public class Globals {
     // Base concept from https://stackoverflow.com/questions/1944656/android-global-variable
 
-    public static final String MQTT_SERVER_URI = "tcp://10.0.0.47";
+    public static final String MQTT_SERVER_URI = "tcp://10.0.0.47:1883";
     public static final String MQTT_CLIENT_ID = MqttAsyncClient.generateClientId(); //"candroid";
     public static ArrayList<HashMap<String, String>> lookupTables = new ArrayList<>();
 }
